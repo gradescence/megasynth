fa3bc9a711ce4c894019c478d5936e827abe0746
Cade <cadee@unr.edu>
2025-12-13 11:09:07 -0800

    Code Names

--- src/megasynth/megasynth.ino
+++ src/megasynth/megasynth.ino
@@ -1,3 +1,7 @@
+//Mikeal Macera --- mmacera@unr.edu
+//Mattia Granata --- mattiag@unr.edu
+//Julian Estorga --- jestorga@unr.edu
+//Cade Evans --- cadee@unr.edu
 #include "KeyMatrixScanner.hpp"
 #include "ToneGenerator.hpp"
 #include "Time.hpp"
